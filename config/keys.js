981b86b43a031abf13796077936ba0113b5a95e0
ajana13 <ajana@umass.edu>
2020-07-18 01:48:15 -0400

    Hosting

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURI: "mongodb://localhost:27017/app1",
-    secret: "yoursecret"
+if (process.env.NODE_ENV == 'production') {
+    module.exports = {
+        mongoURI: "mongodb+srv://admin:<Passw0rd!>@app.znsa8.mongodb.net/<dbname>?retryWrites=true&w=majority",
+        secret: "yoursecret"
+    };
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/app1",
+        secret: "yoursecret"
+    };
 }
