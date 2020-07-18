3395fa168376a2fc3b9dafd260d076a8bc952981
ajana13 <ajana@umass.edu>
2020-07-18 02:07:46 -0400

    Hosting

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == 'production') {
     module.exports = {
-        mongoURI: "mongodb+srv://admin:<Passw0rd%21>@app.znsa8.mongodb.net/<app1>?retryWrites=true&w=majority",
+        mongoURI: "mongodb+srv://admin:<Passw0rd%21>@app.znsa8.mongodb.net/<app1.app>?retryWrites=true&w=majority",
         secret: "yoursecret"
     };
 } else {
